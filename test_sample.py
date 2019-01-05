9b6685f28bffa5b698c5080becf517d4ae83ddb5
Ripudaman Flora <ripudamanflora@Ripudamans-MacBook-Pro.local>
2019-01-05 18:47:41 -0500

    Adding testing for authentication

--- test_sample.py
+++ test_sample.py
@@ -1,4 +1,10 @@
-def func(x):
-    return x + 1
-def test_answer():
-    assert func(4) == 5
+import tweepy
+import os
+
+def test_authentication():
+    auth = tweepy.OAuthHandler(os.environ.get('customer_key'), os.environ.get('consumer_secret'))
+    try:
+        redirect_url = auth.get_authorization_url()
+        assert True
+    except tweepy.TweepError:
+        assert 'Error! Failed to get request token.'
